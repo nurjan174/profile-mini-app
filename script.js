5e4b630fe926333af5a4cae25fa9263fe57b34b0
nurjan174 <rata75758535@gmail.com>
2025-06-02 13:42:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
    window.onload = function() {
      const user = window.Telegram.WebApp.initDataUnsafe.user;
      document.getElementById("photo").src = user.photo_url || "default.jpg";
-     document.getElementById("MODKAbot").innerText = "@" + user.username;
+     document.getElementById("username").innerText = "@" + (user.username || "unknown");
    };
+   
    
